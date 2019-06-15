ce67ce7d9596efda53d45f5f1f7d1d1ba55fdd35
Tim Oliver <me@timoliver.com.au>
2019-06-15 18:43:03 +0900

    Refactored method name

--- TOAlertViewControllerExample/ViewController.m
+++ TOAlertViewControllerExample/ViewController.m
@@ -25,8 +25,8 @@
     TOAlertViewController *alertController = [[TOAlertViewController alloc]
                                               initWithTitle:@"Are you sure?" message:@"This action may take some time to complete. Are you sure you wish to perform this action?"];
 
-    alertController.defaultAction = [TOAlertAction actionWithTitle:@"Yes" action:nil];
-    alertController.cancelAction = [TOAlertAction actionWithTitle:@"Cancel" action:nil];
+    alertController.defaultAction = [TOAlertAction alertActionWithTitle:@"Yes" action:nil];
+    alertController.cancelAction = [TOAlertAction alertActionWithTitle:@"Cancel" action:nil];
 
     [self presentViewController:alertController animated:YES completion:nil];
 }
