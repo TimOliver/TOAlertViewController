b86dce6672f0d975eda6391f49dd07e42cc25a1b
Tim Oliver <git@tim.dev>
2026-06-03 13:58:22 +0900

    Add Terms of Service link demo to the example app

--- TOAlertViewControllerExample/ViewController.m
+++ TOAlertViewControllerExample/ViewController.m
@@ -18,6 +18,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
+
+    UIButton *tosButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [tosButton setTitle:@"Show Terms of Service" forState:UIControlStateNormal];
+    [tosButton addTarget:self action:@selector(showTermsAlert:) forControlEvents:UIControlEventTouchUpInside];
+    tosButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:tosButton];
+    [NSLayoutConstraint activateConstraints:@[
+        [tosButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [tosButton.topAnchor constraintEqualToAnchor:self.view.centerYAnchor constant:60.0f]
+    ]];
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
@@ -38,6 +48,29 @@
 
     // The alert automatically follows the system light/dark appearance.
     [self presentViewController:alertController animated:YES completion:nil];
+}
+
+- (void)showTermsAlert:(id)sender {
+    NSString *body = @"To continue using this app, you must agree to the new Terms of Service. "
+                      "Do you agree to the Terms of Service? If you choose not to agree, the app will close.\n\n"
+                      "Terms of Service";
+    NSMutableAttributedString *message = [[NSMutableAttributedString alloc] initWithString:body];
+    NSRange linkRange = [body rangeOfString:@"Terms of Service" options:NSBackwardsSearch];
+    [message addAttribute:NSLinkAttributeName value:[NSURL URLWithString:@"https://example.com/terms"] range:linkRange];
+    [message addAttribute:NSForegroundColorAttributeName value:UIColor.systemPinkColor range:linkRange];
+    [message addAttribute:NSUnderlineStyleAttributeName value:@(NSUnderlineStyleSingle) range:linkRange];
+
+    TOAlertViewController *alert = [[TOAlertViewController alloc] initWithTitle:@"Terms of Service updated"
+                                                                       message:@""];
+    alert.attributedMessage = message;
+    alert.messageTextAlignment = NSTextAlignmentLeft;
+    alert.linkTappedHandler = ^(NSURL *url, NSRange range) {
+        NSLog(@"Tapped link: %@", url);
+        [UIApplication.sharedApplication openURL:url options:@{} completionHandler:nil];
+    };
+    alert.defaultAction = [TOAlertAction alertActionWithTitle:@"Agree" action:^{ NSLog(@"Agreed"); }];
+    alert.cancelAction = [TOAlertAction alertActionWithTitle:@"Decline" action:^{ NSLog(@"Declined"); }];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 @end
