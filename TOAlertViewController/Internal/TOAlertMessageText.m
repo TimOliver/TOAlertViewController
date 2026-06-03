23ed673b20c40c43e2772ea9f354f99cf1175a14
Tim Oliver <git@tim.dev>
2026-06-03 13:42:55 +0900

    Add TextKit link helper and message normalization with tests

---
+++ TOAlertViewController/Internal/TOAlertMessageText.m
@@ -0,0 +1,172 @@
+//
+//  TOAlertMessageText.m
+//
+//  Copyright 2019-2026 Timothy Oliver. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import "TOAlertMessageText.h"
+
+@implementation TOAlertLink
+
+- (instancetype)initWithURL:(NSURL *)URL range:(NSRange)range {
+    if (self = [super init]) {
+        _URL = URL;
+        _range = range;
+    }
+    return self;
+}
+
+@end
+
+// -------------------------------------------
+
+@interface TOAlertLinkLayout ()
+@property (nonatomic, strong) NSTextStorage *textStorage;
+@property (nonatomic, strong) NSLayoutManager *layoutManager;
+@property (nonatomic, strong) NSTextContainer *textContainer;
+@end
+
+@implementation TOAlertLinkLayout
+
+- (instancetype)initWithAttributedString:(NSAttributedString *)attributedString
+                                    size:(CGSize)size
+                           numberOfLines:(NSInteger)numberOfLines
+                           lineBreakMode:(NSLineBreakMode)lineBreakMode {
+    if (self = [super init]) {
+        _textStorage = [[NSTextStorage alloc] initWithAttributedString:attributedString];
+        _layoutManager = [[NSLayoutManager alloc] init];
+        _textContainer = [[NSTextContainer alloc] initWithSize:size];
+
+        // Match how UILabel lays out: no padding, the label's line count and
+        // break mode.
+        _textContainer.lineFragmentPadding = 0.0f;
+        _textContainer.maximumNumberOfLines = numberOfLines;
+        _textContainer.lineBreakMode = lineBreakMode;
+
+        [_layoutManager addTextContainer:_textContainer];
+        [_textStorage addLayoutManager:_layoutManager];
+
+        // Force layout up front so geometry queries below are valid.
+        [_layoutManager ensureLayoutForTextContainer:_textContainer];
+    }
+    return self;
+}
+
+- (nullable TOAlertLink *)linkAtPoint:(CGPoint)point {
+    if (self.textStorage.length == 0) { return nil; }
+
+    CGPoint adjusted = [self pointAdjustedForVerticalCentering:point];
+
+    CGFloat fraction = 0.0f;
+    NSUInteger glyphIndex = [self.layoutManager glyphIndexForPoint:adjusted
+                                                   inTextContainer:self.textContainer
+                                    fractionOfDistanceThroughGlyph:&fraction];
+
+    // glyphIndexForPoint clamps to the nearest glyph; reject points that fall
+    // beyond the end of the tapped glyph (i.e. past the text entirely).
+    if (fraction >= 1.0f) { return nil; }
+
+    NSUInteger charIndex = [self.layoutManager characterIndexForGlyphAtIndex:glyphIndex];
+    if (charIndex >= self.textStorage.length) { return nil; }
+
+    NSRange range = NSMakeRange(0, 0);
+    id value = [self.textStorage attribute:NSLinkAttributeName
+                                   atIndex:charIndex
+                            effectiveRange:&range];
+    NSURL *URL = [TOAlertLinkLayout URLFromLinkAttributeValue:value];
+    if (URL == nil) { return nil; }
+
+    return [[TOAlertLink alloc] initWithURL:URL range:range];
+}
+
+- (NSArray<NSValue *> *)enclosingRectsForRange:(NSRange)range {
+    NSMutableArray<NSValue *> *rects = [NSMutableArray array];
+    NSRange glyphRange = [self.layoutManager glyphRangeForCharacterRange:range
+                                                   actualCharacterRange:NULL];
+    CGFloat yOffset = [self verticalCenteringOffset];
+
+    [self.layoutManager enumerateEnclosingRectsForGlyphRange:glyphRange
+                                   withinSelectedGlyphRange:NSMakeRange(NSNotFound, 0)
+                                            inTextContainer:self.textContainer
+                                                 usingBlock:^(CGRect rect, BOOL *stop) {
+        rect.origin.y += yOffset;
+        [rects addObject:[NSValue valueWithCGRect:rect]];
+    }];
+
+    return rects;
+}
+
+// UILabel vertically centers text shorter than its bounds. Apply the same
+// offset so points/rects map into the label's coordinate space.
+- (CGFloat)verticalCenteringOffset {
+    CGRect usedRect = [self.layoutManager usedRectForTextContainer:self.textContainer];
+    CGFloat slack = self.textContainer.size.height - usedRect.size.height;
+    return MAX(0.0f, slack * 0.5f);
+}
+
+- (CGPoint)pointAdjustedForVerticalCentering:(CGPoint)point {
+    return CGPointMake(point.x, point.y - [self verticalCenteringOffset]);
+}
+
+// NSLinkAttributeName values may be an NSURL or an NSString per Apple's docs.
++ (nullable NSURL *)URLFromLinkAttributeValue:(nullable id)value {
+    if ([value isKindOfClass:[NSURL class]]) { return (NSURL *)value; }
+    if ([value isKindOfClass:[NSString class]]) { return [NSURL URLWithString:(NSString *)value]; }
+    return nil;
+}
+
+@end
+
+// -------------------------------------------
+
+NSAttributedString *TOAlertNormalizedMessage(NSAttributedString *message,
+                                             UIFont *defaultFont,
+                                             UIColor *defaultColor,
+                                             NSTextAlignment alignment) {
+    NSMutableAttributedString *result = [message mutableCopy];
+    NSRange fullRange = NSMakeRange(0, result.length);
+    if (fullRange.length == 0) { return result; }
+
+    [result beginEditing];
+
+    // Fill in the default font where none is set.
+    [result enumerateAttribute:NSFontAttributeName inRange:fullRange options:0
+                    usingBlock:^(id value, NSRange range, BOOL *stop) {
+        if (value == nil) { [result addAttribute:NSFontAttributeName value:defaultFont range:range]; }
+    }];
+
+    // Fill in the default color where none is set.
+    [result enumerateAttribute:NSForegroundColorAttributeName inRange:fullRange options:0
+                    usingBlock:^(id value, NSRange range, BOOL *stop) {
+        if (value == nil) { [result addAttribute:NSForegroundColorAttributeName value:defaultColor range:range]; }
+    }];
+
+    // Apply alignment only to ranges that carry no paragraph style of their own.
+    [result enumerateAttribute:NSParagraphStyleAttributeName inRange:fullRange options:0
+                    usingBlock:^(id value, NSRange range, BOOL *stop) {
+        if (value == nil) {
+            NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
+            style.alignment = alignment;
+            [result addAttribute:NSParagraphStyleAttributeName value:style range:range];
+        }
+    }];
+
+    [result endEditing];
+    return result;
+}
---
+++ TOAlertViewControllerTests/TOAlertMessageTextTests.m
@@ -0,0 +1,120 @@
+//
+//  TOAlertMessageTextTests.m
+//
+//  Copyright 2019-2026 Timothy Oliver. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import <XCTest/XCTest.h>
+#import "TOAlertMessageText.h"
+
+@interface TOAlertMessageTextTests : XCTestCase
+@end
+
+@implementation TOAlertMessageTextTests
+
+#pragma mark - Normalization -
+
+- (void)testNormalizedMessageFillsDefaultFontColorAndAlignment {
+    NSAttributedString *source = [[NSAttributedString alloc] initWithString:@"Hello"];
+    UIFont *font = [UIFont systemFontOfSize:17.0f];
+    NSAttributedString *result = TOAlertNormalizedMessage(source, font, UIColor.redColor, NSTextAlignmentCenter);
+
+    NSDictionary *attrs = [result attributesAtIndex:0 effectiveRange:NULL];
+    XCTAssertEqualObjects(attrs[NSFontAttributeName], font);
+    XCTAssertEqualObjects(attrs[NSForegroundColorAttributeName], UIColor.redColor);
+    NSParagraphStyle *style = attrs[NSParagraphStyleAttributeName];
+    XCTAssertEqual(style.alignment, NSTextAlignmentCenter);
+}
+
+- (void)testNormalizedMessagePreservesExplicitColor {
+    NSMutableAttributedString *source = [[NSMutableAttributedString alloc] initWithString:@"AB"];
+    [source addAttribute:NSForegroundColorAttributeName value:UIColor.blueColor range:NSMakeRange(0, 1)];
+    NSAttributedString *result = TOAlertNormalizedMessage(source, [UIFont systemFontOfSize:17.0f],
+                                                          UIColor.redColor, NSTextAlignmentLeft);
+
+    XCTAssertEqualObjects([result attribute:NSForegroundColorAttributeName atIndex:0 effectiveRange:NULL],
+                          UIColor.blueColor);
+    XCTAssertEqualObjects([result attribute:NSForegroundColorAttributeName atIndex:1 effectiveRange:NULL],
+                          UIColor.redColor);
+}
+
+#pragma mark - Link hit-testing -
+
+// Build a normalized attributed string with a link over a known substring,
+// laid out at a fixed size. Helpers derive hit points from the layout itself so
+// the tests don't depend on exact font metrics.
+- (TOAlertLinkLayout *)layoutForString:(NSString *)string
+                             linkRange:(NSRange)linkRange
+                                   URL:(id)URLValue
+                                  size:(CGSize)size {
+    NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:string];
+    [text addAttribute:NSLinkAttributeName value:URLValue range:linkRange];
+    NSAttributedString *normalized = TOAlertNormalizedMessage(text, [UIFont systemFontOfSize:17.0f],
+                                                              UIColor.blackColor, NSTextAlignmentLeft);
+    return [[TOAlertLinkLayout alloc] initWithAttributedString:normalized
+                                                         size:size
+                                                numberOfLines:0
+                                                lineBreakMode:NSLineBreakByWordWrapping];
+}
+
+- (void)testLinkAtPointFindsLink {
+    NSString *string = @"Tap Terms here";
+    NSRange linkRange = [string rangeOfString:@"Terms"];
+    NSURL *URL = [NSURL URLWithString:@"https://example.com/terms"];
+    TOAlertLinkLayout *layout = [self layoutForString:string linkRange:linkRange URL:URL size:CGSizeMake(300, 200)];
+
+    NSArray<NSValue *> *rects = [layout enclosingRectsForRange:linkRange];
+    XCTAssertGreaterThan(rects.count, 0);
+    CGRect rect = rects.firstObject.CGRectValue;
+    CGPoint center = CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect));
+
+    TOAlertLink *link = [layout linkAtPoint:center];
+    XCTAssertNotNil(link);
+    XCTAssertEqualObjects(link.URL, URL);
+    XCTAssertTrue(NSEqualRanges(link.range, linkRange));
+}
+
+- (void)testLinkAtPointReturnsNilOutsideLink {
+    NSString *string = @"Tap Terms here";
+    NSRange linkRange = [string rangeOfString:@"Terms"];
+    NSURL *URL = [NSURL URLWithString:@"https://example.com/terms"];
+    TOAlertLinkLayout *layout = [self layoutForString:string linkRange:linkRange URL:URL size:CGSizeMake(300, 200)];
+
+    // Center of the non-link word "Tap" should not resolve to the link.
+    NSRange nonLink = [string rangeOfString:@"Tap"];
+    CGRect rect = [layout enclosingRectsForRange:nonLink].firstObject.CGRectValue;
+    CGPoint center = CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect));
+
+    XCTAssertNil([layout linkAtPoint:center]);
+}
+
+- (void)testLinkAtPointResolvesStringValue {
+    NSString *string = @"Tap Terms here";
+    NSRange linkRange = [string rangeOfString:@"Terms"];
+    // NSLinkAttributeName carrying a plain string rather than an NSURL.
+    TOAlertLinkLayout *layout = [self layoutForString:string linkRange:linkRange
+                                                  URL:@"https://example.com/terms" size:CGSizeMake(300, 200)];
+
+    CGRect rect = [layout enclosingRectsForRange:linkRange].firstObject.CGRectValue;
+    TOAlertLink *link = [layout linkAtPoint:CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect))];
+    XCTAssertNotNil(link);
+    XCTAssertEqualObjects(link.URL.absoluteString, @"https://example.com/terms");
+}
+
+@end
